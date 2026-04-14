4662cf2dd81026a91aae51dfdbb4f5766b80994d
amarjit kumar <amarjit.kumar.11032001@gmail.com>
2026-04-14 22:48:43 +0530

    ADDITION

---
+++ chapter1_module_pip/comments_libraries.py
@@ -0,0 +1,31 @@
+#all libraries
+'''
+Built in Libraries
+
+math → mathematical functions (sqrt, factorial, etc.)
+random → generate random numbers/choices
+datetime → work with date and time
+os → interact with operating system (files, folders)
+sys → system-level operations (args, Python version)
+time → time delays, timestamps
+json → read/write JSON data
+re → pattern matching (regular expressions)
+collections → advanced data structures (Counter, deque)
+itertools → efficient looping tools
+
+'''
+
+'''popular external library
+
+NumPy → fast arrays & numerical operations
+Pandas → data handling (tables like Excel)
+Matplotlib → basic graphs & plots
+Seaborn → advanced, prettier graphs
+Requests → send HTTP requests (APIs)
+Flask → lightweight web apps
+Django → large web applications
+Scikit-learn → ML models (regression, classification)
+TensorFlow → deep learning & AI
+PyTorch → deep learning (popular in research)
+
+'''
