dc11a17ec77285f1efd9310b08b02184f2b6f1c5
amarjit kumar <amarjit.kumar.11032001@gmail.com>
2026-04-23 23:55:43 +0530

    functions

---
+++ 8.functions/1_function.py
@@ -0,0 +1,18 @@
+'''a=12
+b=45
+c=56
+
+avg=(a+b+c)/3
+print(avg)
+
+'''
+# function defination
+def avg():
+	a=int(input())
+	b=int(input())
+	c=int(input())
+	average=(a+b+c)/3
+	print(average)
+
+# function call
+avg()
---
+++ 8.functions/3_function_with_argument.py
@@ -0,0 +1,12 @@
+def goodday(name,ending):
+	print("Goodday!" +name)
+	print(ending)
+	return "ok"
+
+
+goodday("Amar","Thankyou")
+goodday("Rohan","Thankyou")
+goodday("Divya","Thankyou")
+
+a=goodday("Divya","Thankyou")
+print(a)
